7461f0d5c573a00b03295106630d2f15ea5f03b9
Debasish <debasishkumardas5@gmail.com>
2018-10-09 18:02:16 +0530

    Changed the Comment After successful Login.

--- app/src/main/java/com/debasish/mvparc/MainActivity.java
+++ app/src/main/java/com/debasish/mvparc/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements
             }
         });
 
-
+        Toast.makeText(this, "Newly Added.", Toast.LENGTH_SHORT).show();
     }
 
     @Override
